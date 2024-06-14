680184e7f026fed6babc1ac8c4a8d83ee6c47f81
Aswin krishna <85611393+jr-boney@users.noreply.github.com>
2024-06-14 22:06:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,58 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage';
-import JobPage, { jobLoader } from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
-import EditJobPage from './pages/EditJobPage';
-
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobsListing from './components/JobsListing'
 const App = () => {
-  // Add New Job
-  const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newJob),
-    });
-    return;
-  };
+  return (
+    <>
+    <Navbar/>
+<Hero />
+    <HomeCards />
+    <JobsListing/>
+    {/* <!-- Developers and Employers --> */}
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Developers</h2>
+            <p className="mt-2 mb-4">
+              Browse our React jobs and start your career today
+            </p>
+            <a
+              href="/jobs.html"
+              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+            >
+              Browse Jobs
+            </a>
+          </div>
+          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Employers</h2>
+            <p className="mt-2 mb-4">
+              List your job to find the perfect developer for the role
+            </p>
+            <a
+              href="/add-job.html"
+              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+            >
+              Add Job
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
 
-  // Delete Job
-  const deleteJob = async (id) => {
-    const res = await fetch(`/api/jobs/${id}`, {
-      method: 'DELETE',
-    });
-    return;
-  };
+   
+    
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
 
-  // Update Job
-  const updateJob = async (job) => {
-    const res = await fetch(`/api/jobs/${job.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(job),
-    });
-    return;
-  };
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route
-          path='/edit-job/:id'
-          element={<EditJobPage updateJobSubmit={updateJob} />}
-          loader={jobLoader}
-        />
-        <Route
-          path='/jobs/:id'
-          element={<JobPage deleteJob={deleteJob} />}
-          loader={jobLoader}
-        />
-        <Route path='*' element={<NotFoundPage />} />
-      </Route>
-    )
-  );
-
-  return <RouterProvider router={router} />;
-};
-export default App;
+export default App
